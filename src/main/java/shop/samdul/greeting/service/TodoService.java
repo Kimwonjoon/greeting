3b24feff032ec1806a3fa9954e4d4f82ac26a585
Kimwonjoon <kimpass189@gmail.com>
2024-10-23 09:40:51 +0900

    TodoService 수정

--- src/main/java/shop/samdul/greeting/service/TodoService.java
+++ src/main/java/shop/samdul/greeting/service/TodoService.java
@@ -9,13 +9,18 @@ import java.util.List;
 
 @Service
 public class TodoService {
+
     @Autowired
     TodoMapper todoMapper;
 
-    public List<TodoEntity> getTools() {
+    public List<TodoEntity> getTodos() {
         System.out.println("[service] findAll");
         List<TodoEntity> todos = todoMapper.findAll();
         System.out.println("[todos]:" + todos.size());
         return todos;
+    }
+
+    public TodoEntity findById(Integer id) {
+        return todoMapper.findById(id);
     }
 }
